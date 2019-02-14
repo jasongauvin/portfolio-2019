6706596b732cf6487cb7e04148940d03f002f26c
jasongauvin <contact.jason.gauvin@gmail.com>
2019-02-14 01:03:37 +0100

    2019-02-14

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 import './styles/master.scss'; 
-console.log('bite');
+import  Typed  from  'typed.js' ;
+console.log('mordre');
+
+// Code for typewrite-machine with typed-js
+var typed3 = new Typed('#typed', {
+strings: ['développeur web junior', 'chargé de communication', 'attaché de presse'],
+  typeSpeed: 120,
+  backSpeed: 0,
+  smartBackspace: false, // this is a default
+  loop: true,
+  fadeOut: false
+});
+  
+
